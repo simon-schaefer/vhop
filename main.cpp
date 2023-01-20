e4c493d0da135d114ad198e33bb3b6ea5ab3008d
simon-schaefer <simon.k.schaefer@gmail.com>
2023-01-20 21:14:33 +0100

    initialization with vposer mean

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 int main(int argc, char** argv) {
     cnpy::npz_t npz = cnpy::npz_load("../data/test/sample.npz");
+    cnpy::npz_t npz_means = cnpy::npz_load("../data/vposer_mean.npz");
     vhop::beta_t<double> beta = vhop::utility::loadDoubleMatrix(npz.at("betas"), vhop::SHAPE_BASIS_DIM, 1);
     Eigen::Matrix3d K = vhop::utility::loadDoubleMatrix(npz.at("intrinsics"), 3, 3);
     Eigen::Matrix4d T_C_B = vhop::utility::loadDoubleMatrix(npz.at("T_C_B"), 4, 4);
@@ -17,6 +18,7 @@ int main(int argc, char** argv) {
     vhop::joint_op_scores_t joints_2d_scores = vhop::utility::loadDoubleMatrix(npz.at("keypoints_2d_scores"), vhop::JOINT_NUM_OP, 1);
 
     vhop::SMPL smpl_model("../data/smpl_neutral.npz");
+    // vhop::theta_t<double> pose = vhop::utility::loadDoubleMatrix(npz_means.at("thetas"), vhop::THETA_DIM, 1);
     vhop::theta_t<double> pose = vhop::theta_t<double>::Zero();
     ceres::Problem problem;
     ceres::LossFunction* loss_function = new ceres::CauchyLoss(1.0);
