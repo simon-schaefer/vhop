8291bb7c9fc8fff777f2ae226624684f5b0abdb0
simon-schaefer <simon.k.schaefer@gmail.com>
2023-01-22 02:01:16 +0100

    minor restructuring

--- apps/ceres_vposer.cpp
+++ apps/ceres_vposer.cpp
@@ -9,8 +9,51 @@
 #include "vposer/constants.h"
 #include "vposer/VPoser.h"
 
-#include "vhop/residuals/rp_smpl.h"
-#include "vhop/residuals/rp_vposer.h"
+
+class ReprojectionError {
+
+ public:
+
+  ReprojectionError(
+      const vhop::beta_t<double> &beta,
+      const VPoser& vposer,
+      const Eigen::Matrix3d &K,
+      const Eigen::Matrix4d &T_C_B,
+      const vhop::joint_op_2d_t<double> &joint_kps,
+      const vhop::joint_op_scores_t &kp_scores,
+      const vhop::SMPL &smpl_model)
+      : beta_(beta),
+        vposer_(vposer),
+        T_C_B_(T_C_B),
+        K_(K),
+        joint_kps_(joint_kps),
+        joint_kps_scores_(kp_scores),
+        smpl_model_(smpl_model) {}
+
+  bool operator()(const double *latentZ, double *reprojection_error) const {
+      vposer::latent_t<double> z(latentZ);
+      vhop::rotMats_t<double> rotMats = vposer_.decode(z, true);
+
+      vhop::joint_op_2d_t<double> joints2d;
+      smpl_model_.ComputeOpenPoseKP<double>(beta_, rotMats, T_C_B_, K_, &joints2d);
+
+      for (int i = 0; i < vhop::JOINT_NUM_OP; ++i) {
+          double score = joint_kps_scores_(i);
+          reprojection_error[i * 2] = score * (joints2d(i, 0) - joint_kps_(i, 0));
+          reprojection_error[i * 2 + 1] = score * (joints2d(i, 1) - joint_kps_(i, 1));
+      }
+      return true;
+  }
+
+ private:
+  vhop::beta_t<double> beta_;
+  VPoser vposer_;
+  Eigen::Matrix3d K_;
+  Eigen::Matrix4d T_C_B_;
+  vhop::joint_op_2d_t<double> joint_kps_;
+  vhop::joint_op_scores_t joint_kps_scores_;
+  vhop::SMPL smpl_model_;
+};
 
 
 int main(int argc, char** argv) {
@@ -28,17 +71,9 @@ int main(int argc, char** argv) {
     ceres::Problem problem;
     ceres::LossFunction* loss_function = new ceres::CauchyLoss(1.0);
     ceres::CostFunction* cost_function = new ceres::NumericDiffCostFunction<
-        vhop::ReprojectionErrorVPoser, ceres::CENTRAL, vhop::JOINT_NUM_OP * 2, vposer::LATENT_DIM>(
-        new vhop::ReprojectionErrorVPoser(beta, vposer_model, K, T_C_B, joints_2d_gt, joints_2d_scores, smpl_model));
+        ReprojectionError, ceres::CENTRAL, vhop::JOINT_NUM_OP * 2, vposer::LATENT_DIM>(
+        new ReprojectionError(beta, vposer_model, K, T_C_B, joints_2d_gt, joints_2d_scores, smpl_model));
     problem.AddResidualBlock(cost_function, loss_function, z.data());
-
-//    vhop::theta_t<double> pose = vhop::theta_t<double>::Zero();
-//    ceres::Problem problem;
-//    ceres::LossFunction* loss_function = new ceres::CauchyLoss(1.0);
-//    ceres::CostFunction* cost_function = new ceres::NumericDiffCostFunction<
-//        vhop::ReprojectionErrorSMPL, ceres::CENTRAL, vhop::JOINT_NUM_OP * 2, vhop::JOINT_NUM * 3>(
-//        new vhop::ReprojectionErrorSMPL(beta, K, T_C_B, joints_2d_gt, joints_2d_scores, smpl_model));
-//    problem.AddResidualBlock(cost_function, loss_function, pose.data());
 
     ceres::Solver::Options options;
     options.max_num_iterations = 40;
@@ -50,7 +85,6 @@ int main(int argc, char** argv) {
     vhop::joint_op_2d_t<double> joints_2d;
     vhop::rotMats_t<double> rotMats = vposer_model.decode(z);
     smpl_model.ComputeOpenPoseKP(beta, rotMats, T_C_B, K, &joints_2d);
-    // smpl_model.ComputeOpenPoseKP(beta, pose, T_C_B, K, &joints_2d);
     vhop::visualization::drawKeypoints("../data/zju-mocap/sample.jpg",
                                        joints_2d.cast<int>(),
                                        joints_2d_gt.cast<int>(),
