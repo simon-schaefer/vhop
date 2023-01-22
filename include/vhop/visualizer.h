38c8c3a2040693d5f2ec9a5c195b4be599494f00
Omar Faig Orujlu <42292086+OmarFaig@users.noreply.github.com>
2023-01-22 23:44:54 +0100

    working mesh visaulizer
    visualize_mesh is updated and working

--- include/vhop/visualizer.h
+++ include/vhop/visualizer.h
@@ -1,28 +1,29 @@
- 
+#include <easy3d/core/surface_mesh.h>
 #include <easy3d/viewer/viewer.h>
-#include <easy3d/renderer/camera.h>
-#include <easy3d/renderer/drawable_lines.h>
-#include <easy3d/renderer/drawable_points.h>
-#include <easy3d/renderer/drawable_triangles.h>
-#include <easy3d/core/types.h>
-#include <easy3d/util/resource.h>
 #include <easy3d/util/initializer.h>
- 
+#include <easy3d/core/surface_mesh_builder.h>
  
 using namespace easy3d;
-namespace vhop{
+namespace vhop_visualization_{
  
-void visualize_mesh(const std::vector<vec3>& vertices, const std::vector<std::vector<unsigned int>>& faces)  {
+void visualize_mesh(const std::vector<vec3>& vertices, const std::vector<std::vector<int>>&faces)  {
     // Initialize Easy3D.
     initialize();
+    easy3d::SurfaceMesh mesh;
+    SurfaceMeshBuilder builder(&mesh);
+    builder.begin_surface();
+    Viewer viewer("Mesh ");//name of the opened window
+    for (int f=0;f<faces.size(); f++){
 
-    Viewer viewer("Tutorial_301_Drawables");
-    auto surface = new TrianglesDrawable("faces");
-    surface->update_vertex_buffer(vertices);
-    surface->update_element_buffer(faces);
-    viewer.add_drawable(surface);
-    viewer.fit_screen();
-     viewer.run();
+   SurfaceMesh::Vertex v0 = builder.add_vertex(vertices[faces[f][0]]);
+   SurfaceMesh::Vertex v1 = builder.add_vertex(vertices[faces[f][1]]);
+   SurfaceMesh::Vertex v2 = builder.add_vertex(vertices[faces[f][2]]);
+   builder.add_triangle(v0, v1, v2);//create triangles with vertices and faces
+  }
+   builder.end_surface(false);
+   viewer.add_model(&mesh);
+   viewer.fit_screen();
+   viewer.run();
 
      
 }
